9f63102480e297c37cd8c7aae1e8dcb02ba0faaa
zhaokai04 <zhaokai04@qianxin.com>
2024-04-28 10:28:10 +0000

    add

--- gost/serverstop/main.go
+++ gost/serverstop/main.go
@@ -1,1 +1,23 @@
 package main
+
+import (
+	"context"
+	"fmt"
+	"sync"
+)
+
+var serverwg sync.WaitGroup
+
+func worker(ctx context.Context) {
+	serverwg.Add(1)
+	defer serverwg.Done()
+
+	for {
+		select {
+		case <-ctx.Done():
+			fmt.Println("recv context done, grouting exit")
+		default:
+			fmt.Println("working")
+		}
+	}
+}
